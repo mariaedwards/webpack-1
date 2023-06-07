46f7247cf9eb20005fee2bccfca3cc57b9b07fb8
maria.edwards <maria.ana.edwards@gmail.com>
2023-06-07 15:15:42 -0700

    Adds default setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'), // code splitting
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js', // same as entry point key
+  },
+  module: {
+    // loaders
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/template.html'),
+    }),
+  ],
+};
